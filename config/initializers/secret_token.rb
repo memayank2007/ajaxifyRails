70db3134c44b874066689e48001bae754fbce029
Mayank Mishra <memayank2007@gmail.com>
2014-03-29 22:33:38 +0530

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AjaxifyRails::Application.config.secret_key_base = '712251f0b93402035b75b50ef1ea274f325e359952e5c59244d756104a0043821c62aaf758331c4e6dd8a0fba292d679aae9117564d91b5cdf96dc16108352bc'
